dd4a0f6f0de3e5a084c17f78476294a0e05bcd90
figsoda <figsoda@pm.me>
2020-12-09 23:08:46 -0500

    patch xtrt to use nix-shell

--- pkgs/xtrt/default.nix
+++ pkgs/xtrt/default.nix
@@ -4,6 +4,7 @@ stdenv.mkDerivation {
   name = "xtrt";
   src = sources.xtrt;
   installPhase = ''
+    sed -i '1s/#!.*/#!\/usr\/bin\/env nix-shell\n#!nix-shell -i bash -p gnutar gzip unzip xz/' xtrt
     mkdir -p $out/bin;
     cp xtrt $out/bin;
   '';
