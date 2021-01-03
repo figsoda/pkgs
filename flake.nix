4d42ef00f9e0c1d231281462b0d5b698e1733016
figsoda <figsoda@pm.me>
2021-01-02 21:43:11 -0500

    use the same overlay for every system

--- flake.nix
+++ flake.nix
@@ -41,68 +41,68 @@
   };
 
   outputs = { flake-utils, naersk, nixpkgs, nixpkgs-mozilla, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      with builtins;
-      let
-        sources = (fromJSON (readFile ./flake.lock)).nodes;
-        pkgs = nixpkgs.legacyPackages.${system};
-        mozilla = pkgs.callPackage "${nixpkgs-mozilla}/package-set.nix" { };
-        rustNightly = (mozilla.rustChannelOf {
-          date = "2020-12-30";
-          channel = "nightly";
-          sha256 = "f2/rc0jI1wzvU/b82nyR+ehLtuOjLv7TjnCmtbUClcA=";
-        }).rust;
-      in rec {
-        defaultPackage = packages;
+    let
+      outputs = flake-utils.lib.eachDefaultSystem (system:
+        with builtins;
+        let
+          sources = (fromJSON (readFile ./flake.lock)).nodes;
+          pkgs = nixpkgs.legacyPackages.${system};
+          mozilla = pkgs.callPackage "${nixpkgs-mozilla}/package-set.nix" { };
+          rustNightly = (mozilla.rustChannelOf {
+            date = "2020-12-30";
+            channel = "nightly";
+            sha256 = "f2/rc0jI1wzvU/b82nyR+ehLtuOjLv7TjnCmtbUClcA=";
+          }).rust;
+        in rec {
+          defaultPackage = packages;
 
-        packages = {
-          luaformatter = pkgs.stdenv.mkDerivation {
-            pname = "luaformatter";
-            version = sources.luaformatter.original.ref;
-            src = inputs.luaformatter;
-            buildInputs = [ pkgs.cmake ];
-            configurePhase = ''
-              rmdir third_party/*
-              ln -sT ${inputs.antlr4} third_party/antlr4
-              ln -sT ${inputs.catch2} third_party/Catch2
-              ln -sT ${inputs.args} third_party/args
-              ln -sT ${inputs.yaml-cpp} third_party/yaml-cpp
-              cmake .
-            '';
-            installPhase = ''
-              mkdir -p $out/bin
-              cp lua-format $out/bin
-            '';
+          packages = {
+            luaformatter = pkgs.stdenv.mkDerivation {
+              pname = "luaformatter";
+              version = sources.luaformatter.original.ref;
+              src = inputs.luaformatter;
+              buildInputs = [ pkgs.cmake ];
+              configurePhase = ''
+                rmdir third_party/*
+                ln -sT ${inputs.antlr4} third_party/antlr4
+                ln -sT ${inputs.catch2} third_party/Catch2
+                ln -sT ${inputs.args} third_party/args
+                ln -sT ${inputs.yaml-cpp} third_party/yaml-cpp
+                cmake .
+              '';
+              installPhase = ''
+                mkdir -p $out/bin
+                cp lua-format $out/bin
+              '';
+            };
+
+            mmtc = (naersk.lib.${system}.override {
+              cargo = rustNightly;
+              rustc = rustNightly;
+            }).buildPackage {
+              pname = "mmtc";
+              version = nixpkgs.lib.removePrefix "v" sources.mmtc.original.ref;
+              src = inputs.mmtc;
+            };
+
+            xtrt = pkgs.stdenv.mkDerivation {
+              pname = "xtrt";
+              version = substring 0 7 sources.xtrt.locked.rev;
+              src = inputs.xtrt;
+              patchPhase = ''
+                substituteInPlace xtrt \
+                  --replace "bzip2 " "${pkgs.bzip2}/bin/bzip2 " \
+                  --replace "gzip " "${pkgs.gzip}/bin/gzip " \
+                  --replace "tar " "${pkgs.gnutar}/bin/tar " \
+                  --replace "unzip " "${pkgs.unzip}/bin/unzip " \
+                  --replace "xz " "${pkgs.xz}/bin/xz "
+              '';
+              installPhase = ''
+                mkdir -p $out/bin
+                cp xtrt $out/bin
+              '';
+            };
           };
-
-          mmtc = (naersk.lib.${system}.override {
-            cargo = rustNightly;
-            rustc = rustNightly;
-          }).buildPackage {
-            pname = "mmtc";
-            version = nixpkgs.lib.removePrefix "v" sources.mmtc.original.ref;
-            src = inputs.mmtc;
-          };
-
-          xtrt = pkgs.stdenv.mkDerivation {
-            pname = "xtrt";
-            version = substring 0 7 sources.xtrt.locked.rev;
-            src = inputs.xtrt;
-            patchPhase = ''
-              substituteInPlace xtrt \
-                --replace "bzip2 " "${pkgs.bzip2}/bin/bzip2 " \
-                --replace "gzip " "${pkgs.gzip}/bin/gzip " \
-                --replace "tar " "${pkgs.gnutar}/bin/tar " \
-                --replace "unzip " "${pkgs.unzip}/bin/unzip " \
-                --replace "xz " "${pkgs.xz}/bin/xz "
-            '';
-            installPhase = ''
-              mkdir -p $out/bin
-              cp xtrt $out/bin
-            '';
-          };
-        };
-
-        overlay = _: _: packages;
-      });
+        });
+    in outputs // { overlay = _: super: outputs.packages.${super.system}; };
 }
