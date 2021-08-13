08638331e694b4068a295226f3b246732e132538
figsoda <figsoda@pm.me>
2021-08-12 21:23:10 -0400

    remove dependency on flake-utils

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     ymdl = {
       url = "github:figsoda/ymdl";
@@ -8,35 +7,37 @@
     };
   };
 
-  outputs = { flake-utils, nixpkgs, ... }@inputs:
-    with builtins;
-    with flake-utils.lib;
-    let
-      outputs = eachSystem allSystems (system:
-        let
-          sources = (fromJSON (readFile ./flake.lock)).nodes;
-          pkgs = nixpkgs.legacyPackages.${system};
-        in rec {
-          defaultPackage = packages;
+  outputs = { nixpkgs, ... }@inputs: rec {
+    defaultPackage = packages;
 
-          packages = {
-            ymdl = pkgs.stdenv.mkDerivation {
-              pname = "ymdl";
-              version = "unstable-${sources.ymdl.locked.rev}";
-              src = inputs.ymdl;
-              installPhase = ''
-                mkdir -p $out/{bin,lib}
-                cp postdl.py $out/lib
-                substitute {,$out/bin/}ymdl \
-                  --replace "python3 postdl.py" "${
-                    (pkgs.python3.withPackages
-                      (ps: [ ps.pytaglib ])).interpreter
-                  } $out/lib/postdl.py" \
-                  --replace youtube-dl ${pkgs.youtube-dl}/bin/youtube-dl
-                chmod +x $out/bin/ymdl
-              '';
-            };
-          };
-        });
-    in outputs // { overlay = _: super: outputs.packages.${super.system}; };
+    packages = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ] (system:
+      let
+        inherit (nixpkgs.legacyPackages.${system}) python3 stdenv youtube-dl;
+        sources = with builtins; (fromJSON (readFile ./flake.lock)).nodes;
+      in {
+        ymdl = stdenv.mkDerivation {
+          pname = "ymdl";
+          version = "unstable-${sources.ymdl.locked.rev}";
+          src = inputs.ymdl;
+          installPhase = ''
+            mkdir -p $out/{bin,lib}
+            cp postdl.py $out/lib
+            substitute {,$out/bin/}ymdl \
+              --replace "python3 postdl.py" "${
+                (python3.withPackages (ps: [ ps.pytaglib ])).interpreter
+              } $out/lib/postdl.py" \
+              --replace youtube-dl ${youtube-dl}/bin/youtube-dl
+            chmod +x $out/bin/ymdl
+          '';
+        };
+      });
+
+    overlay = _: super: packages.${super.system};
+  };
 }
