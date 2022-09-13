bea3cdb16d3c6e6cb69464d55c827dba0ec23e1c
figsoda <figsoda@pm.me>
2022-09-13 11:36:30 -0400

    format

--- flake.nix
+++ flake.nix
@@ -24,19 +24,27 @@
           ant jdk11 jre makeWrapper python3 sd stdenv yt-dlp;
         date = input: builtins.substring 0 8 input.lastModifiedDate;
       in
+
       {
         rust-templates = stdenv.mkDerivation {
           pname = "rust-templates";
           version = date rust-templates;
+
           src = rust-templates;
+
           installPhase = ''
+            runHook preInstall
+
             mkdir -p $out/{bin,lib}
             cp -r bin binlib lib $out/lib
             substitute {,$out/bin/}generate \
               --replace {,$out/lib/}\$1 \
               --replace {,${sd}/bin/}sd
             chmod +x $out/bin/generate
+
+            runHook postInstall
           '';
+
           meta.mainProgram = "generate";
         };
 
@@ -76,8 +84,12 @@
         ymdl = stdenv.mkDerivation {
           pname = "ymdl";
           version = date ymdl;
+
           src = ymdl;
+
           installPhase = ''
+            runHook preInstall
+
             mkdir -p $out/{bin,lib}
             cp postdl.py $out/lib
             substitute {,$out/bin/}ymdl \
@@ -86,6 +98,8 @@
               } $out/lib/postdl.py" \
               --replace yt-dlp ${yt-dlp}/bin/yt-dlp
             chmod +x $out/bin/ymdl
+
+            runHook postInstall
           '';
         };
       });
